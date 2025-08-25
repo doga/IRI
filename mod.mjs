7fed2e981772c20bf1f04d2f15c347ea8e336c8f
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2025-08-25 20:03:34 +0200

    add jsdoc to module file

--- mod.mjs
+++ mod.mjs
@@ -1,3 +1,11 @@
+/**
+ * IRI is a library for parsing URLs and URNs.
+ * @module IRI <https://esm.sh/gh/doga/IRI@3.1.4/mod.mjs>
+ * @author Doğa Armangil <d.armangil@gmail.com>
+ * @license  Apache-2.0 <https://www.apache.org/licenses/LICENSE-2.0>
+ * @see {@link https://github.com/doga/IRI|GitHub repository}
+ */
+
 export {IRI} from './lib/iri.mjs';
 export {IRL, irl, url} from './lib/irl.mjs';
 export {URN, urn} from './lib/urn.mjs';
