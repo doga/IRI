6ac806a0d94a6dac70e6168be3b0cfadb263b3b7
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2026-04-30 13:39:04 +0200

    move deps to jsr

--- deps.mjs
+++ deps.mjs
@@ -1,1 +1,3 @@
-export { punycode } from 'https://esm.sh/gh/doga/punycode@1.0.0/mod.mjs';
+// export { punycode } from 'https://esm.sh/gh/doga/punycode@1.0.0/mod.mjs';
+export { punycode } from 'https://esm.sh/jsr/@arma/punycode@1.0.1/mod.mjs';
+// https://esm.sh/jsr/@std/encoding@1.0.0/base64
